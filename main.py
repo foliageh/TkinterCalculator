e9b25c08765c104e77dd7b1a15b1ab548ef1aadb
Михаил Муратов <mygazztop1@gmail.com>
2019-05-12 17:17:14 +0700

    упростил код

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from tkinter import *
 from math import *
-
 root = Tk()
 
 ent = Entry(width=100)
 btn = Button(text="Посчитать", height=3, width=30, bg='aqua', font='arial 26')
 lab = Label(bg='black', fg='white', height=3, width=30, font='arial 26')
 
+
 def strToSortlist(event):
     s = ent.get()
-    s.strip()
+    s = s.replace(' ', '')
+    s0 = s
     s = s.replace('^', '**')
     try:
-        lab['text'] = ent.get()+'='+str(eval(s))
+        lab['text'] = s0+'='+str(eval(s))
     except:
         lab['text'] = 'НЕКОРРЕКТНОЕ ВЫРАЖЕНИЕ!'
 
